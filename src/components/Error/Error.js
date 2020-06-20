d1a410302f0774e85619042a2bcd2fd82745be5d
rahul499 <rahul4mishra99@gmail.com>
2020-06-20 17:04:53 +0530

    update Error.js

---
+++ src/components/Error/Error.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Error extends Component{
+	render(){
+return(      
+         <div>
+          <center><h1>Error 404 </h1>
+          <p>The page you are looking for is not available.</p></center>
+          </div>
+   
+
+			);
+	}
+}
+
+export default Error;
